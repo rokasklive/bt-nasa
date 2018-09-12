b52511e254c72671a050fc59cd1cf7b16aacbe7d
bt <bt@bt-Inspiron-1545>
2018-09-12 15:21:24 +0300

    Grid with randomly generated objects

---
+++ src/main/java/test/Ufo.java
@@ -0,0 +1,39 @@
+package test;
+
+public class Ufo {
+
+	private int xCoord;
+	private int yCoord;
+	private boolean discovered;
+
+	public Ufo(int x, int y, boolean disc) {
+		this.xCoord = x;
+		this.yCoord = y;
+		this.discovered = disc;
+	}
+
+	public boolean isDiscovered() {
+		return discovered;
+	}
+
+	public void setDiscovered(boolean discovered) {
+		this.discovered = discovered;
+	}
+
+	public int getxCoord() {
+		return xCoord;
+	}
+
+	public int getyCoord() {
+		return yCoord;
+	}
+
+	public boolean equals(Ufo obj) {
+		if( xCoord == obj.getxCoord() && yCoord == obj.getyCoord() ) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+}

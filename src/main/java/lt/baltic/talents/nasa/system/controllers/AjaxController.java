30d4c776a78da170b43b2057c0b3deb2ce67b763
gediminas <gediminas@gediminas-GS70-6QE>
2018-09-14 15:25:47 +0300

    grid coord from 0 to -1 and Controller to service autowired

--- src/main/java/lt/baltic/talents/nasa/system/controllers/AjaxController.java
+++ src/main/java/lt/baltic/talents/nasa/system/controllers/AjaxController.java
@@ -1,6 +1,7 @@
 package lt.baltic.talents.nasa.system.controllers;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,11 +10,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import lt.baltic.talents.nasa.system.models.Grid;
+import lt.baltic.talents.nasa.system.services.UfoService;
 
 
 @Controller
 @RequestMapping("/findObj")
 public class AjaxController {
+	
+	@Autowired
+	private UfoService ufoService;
 	
 	@RequestMapping(method = RequestMethod.GET)
 	@ResponseBody

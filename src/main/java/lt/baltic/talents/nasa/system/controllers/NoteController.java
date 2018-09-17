4c4b9e7982be20f589373a89f49333651f4c7d70
Gediminas <misev.gediminas@gmail.com>
2018-09-17 17:01:09 +0300

    working save to database

--- src/main/java/lt/baltic/talents/nasa/system/controllers/NoteController.java
+++ src/main/java/lt/baltic/talents/nasa/system/controllers/NoteController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.google.gson.Gson;
 
+import lt.baltic.talents.nasa.system.models.Ufo;
 import lt.baltic.talents.nasa.system.services.UfoService;
 
 @Controller
@@ -40,6 +41,8 @@ public class NoteController {
 		String note = gson.fromJson(text, String.class);
 		String cord = gson.fromJson(coord, String.class);
 		System.out.println("ajax-" + note + "  " + cord);
+		
+		ufoService.note(new Ufo(Integer.parseInt(cord.split("_")[0]), Integer.parseInt(cord.split("_")[1]), true), note);
 		
 		return "success";
 	}
